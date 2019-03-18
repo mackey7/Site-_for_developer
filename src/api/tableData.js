b3974f15e888c90e68e776f3b945d7b1016d6bdc
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-03-18 14:23:50 +0100

    change fetch data

---
+++ src/api/tableData.js
@@ -0,0 +1,12 @@
+export const tableData = [
+    { nr: "P11A", kondygnacja: "parter", powierzchnia: 64.32, dodatek: ' ogródek 31', cena: 289500, status: "wolny" },
+    { nr: "P11B", kondygnacja: "piętro", powierzchnia: 66.57, dodatek: ' strych 15', cena: 259500, status: "rezerwacja" },
+    { nr: "P12A", kondygnacja: "parter", powierzchnia: 64.32, dodatek: ' ogródek 31', cena: 289500, status: "wolny" },
+    { nr: "P12B", kondygnacja: "piętro", powierzchnia: 66.57, dodatek: ' strych 15', cena: 259500, status: "wolny" },
+    { nr: "P13A", kondygnacja: "parter", powierzchnia: 64.32, dodatek: ' ogródek 31', cena: 289500, status: "wolny" },
+    { nr: "P13B", kondygnacja: "piętro", powierzchnia: 66.57, dodatek: ' strych 15', cena: 259500, status: "wolny" },
+    { nr: "P14A", kondygnacja: "parter", powierzchnia: 64.32, dodatek: ' ogródek 31', cena: 289500, status: "wolny" },
+    { nr: "P14B", kondygnacja: "piętro", powierzchnia: 66.57, dodatek: ' strych 15', cena: 259500, status: "wolny" },
+    { nr: "P15A", kondygnacja: "parter", powierzchnia: 64.32, dodatek: ' ogródek 31', cena: 289500, status: "wolny" },
+    { nr: "P15B", kondygnacja: "piętro", powierzchnia: 66.57, dodatek: ' strych 15', cena: 259500, status: "sprzedany" },
+]
--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { tableData } from '../../api/tableData'
 import './table.scss';
 
 
@@ -6,18 +7,7 @@ class Table extends React.Component {
     constructor() {
         super()
         this.state = {
-            ofert: [
-                { nr: "P11A", kondygnacja: "parter", powierzchnia: 64.32, dodatek: ' ogródek 31', cena: 289500, status: "wolny" },
-                { nr: "P11B", kondygnacja: "piętro", powierzchnia: 66.57, dodatek: ' strych 15', cena: 259500, status: "rezerwacja" },
-                { nr: "P12A", kondygnacja: "parter", powierzchnia: 64.32, dodatek: ' ogródek 31', cena: 289500, status: "wolny" },
-                { nr: "P12B", kondygnacja: "piętro", powierzchnia: 66.57, dodatek: ' strych 15', cena: 259500, status: "wolny" },
-                { nr: "P13A", kondygnacja: "parter", powierzchnia: 64.32, dodatek: ' ogródek 31', cena: 289500, status: "wolny" },
-                { nr: "P13B", kondygnacja: "piętro", powierzchnia: 66.57, dodatek: ' strych 15', cena: 259500, status: "wolny" },
-                { nr: "P14A", kondygnacja: "parter", powierzchnia: 64.32, dodatek: ' ogródek 31', cena: 289500, status: "wolny" },
-                { nr: "P14B", kondygnacja: "piętro", powierzchnia: 66.57, dodatek: ' strych 15', cena: 259500, status: "wolny" },
-                { nr: "P15A", kondygnacja: "parter", powierzchnia: 64.32, dodatek: ' ogródek 31', cena: 289500, status: "wolny" },
-                { nr: "P15B", kondygnacja: "piętro", powierzchnia: 66.57, dodatek: ' strych 15', cena: 259500, status: "sprzedany" },
-            ]
+            ofert: tableData
         }
 
     }
