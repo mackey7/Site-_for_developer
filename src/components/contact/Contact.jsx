e8670cf26d4dca786f5b34b0a5efe526c8f0fecc
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-03-18 12:59:44 +0100

    fix style

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -5,39 +5,40 @@ const Contact = () => {
     return (
         <section className="contact">
             <div className="contact_container">
-                <div className="contact_company">
-                    <div className="contact_company-about">
-                        <h2><i class="far fa-envelope"></i> KONTAKT</h2>
-                        <img src={logo} alt="logo" />
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et libero dicta autem illum similique sequi expedita consectetur tenetur maiores, perspiciatis praesentium nobis, earum molestiae nulla architecto, quasi quidem ex? Nesciunt minima cumque, sunt eaque a voluptas officia soluta, amet at tempora doloremque nemo debitis! Voluptatibus quos at, cupiditate nostrum veritatis veniam accusamus distinctio, reprehenderit facilis tempora nemo repudiandae corporis facere.</p>
-                    </div>
-                    <div className="contact_company-contact">
-                        <p>Lorem Ipsum Sp.z o.o.</p>
-                        <p>32-020 Kraków, Floriańska 1/1</p>
-                        <p>
-                            +48 777 77 77
+
+                <div className="contact_container--about">
+                    <h2><i class="far fa-envelope"></i> KONTAKT</h2>
+                    <img src={logo} alt="logo" />
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et libero dicta autem illum similique sequi expedita consectetur tenetur maiores, perspiciatis praesentium nobis, earum molestiae nulla architecto, quasi quidem ex? Nesciunt minima cumque, sunt eaque a voluptas officia soluta, amet at tempora doloremque nemo debitis! Voluptatibus quos at, cupiditate nostrum veritatis veniam accusamus distinctio, reprehenderit facilis tempora nemo repudiandae corporis facere.</p>
+                </div>
+
+                <div className="contact_container--contact">
+                    <p>Lorem Ipsum Sp.z o.o.</p>
+                    <p>32-020 Kraków, Floriańska 1/1</p>
+                    <p>
+                        +48 777 77 77
                    </p>
-                        <p>kontakt@kontakt.com.pl</p>
-                        <p>KRS: 000002222333</p>
-                        <p>nipm: 975-2222-22-22</p>
+                    <p>kontakt@kontakt.com.pl</p>
+                    <p>KRS: 000002222333</p>
+                    <p>nipm: 975-2222-22-22</p>
+                </div>
+
+                <form className="contact_container--form" action="">
+                    <label htmlFor=""></label>
+                    <input type="text" placeholder="Imię i nazwisko" />
+                    <label htmlFor=""></label>
+                    <input type="text" placeholder="e-mail" />
+                    <textarea name="" id="" cols="30" rows="10" placeholder="treść wiadomości"></textarea>
+                    <div className="contact_form-veryficate">
+                        <input type="text" value="AXK^%CH" />
+                        <input type="text" placeholder="przepisz" />
                     </div>
-                </div>
-                <div>
-                    <form className="contact_form" action="">
-                        <label htmlFor=""></label>
-                        <input type="text" />
-                        <label htmlFor=""></label>
-                        <input type="text" />
-                        <textarea name="" id="" cols="30" rows="10"></textarea>
-                        <div className="contact_form-veryficate">
-                            <input type="text" />
-                            <input type="text" />
-                        </div>
-                        <input type="button" value="Wysłanie formularza" />
-                    </form>
-                </div>
+                    <input type="button" value="Wysłanie formularza" />
+                </form>
+
             </div>
-        </section>
+
+        </section >
     )
 }
 export default Contact;
