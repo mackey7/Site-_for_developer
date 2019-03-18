34b98a942bf3e94e705edaa251b7893615e59b38
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-03-18 14:50:04 +0100

    create redux structure

---
+++ src/reducers/reducers.js
@@ -0,0 +1,4 @@
+import { combineReducers } from 'redux';
+import { flatVewsReducer } from './flatVewsReducer'
+
+export const reducers = combineReducers({ flatVewsReducer })
---
+++ src/store/store.js
@@ -0,0 +1,3 @@
+import { createStore } from 'redux';
+import { reducers } from '../reducers/reducers';
+export const store = createStore(reducers)
