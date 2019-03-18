7bbc99341230d0e3e5c1f2120a1bbcbfa437d38d
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-03-18 08:31:48 +0100

    crete table component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Menu from './components/topMenu/Menu.jsx'
 import Header from './components/header/Header.jsx';
 import Contact from './components/contact/Contact.jsx';
 import FlatViews from './components/flatViews/FlatViews.jsx'
+import Table from './components/table/Table.jsx';
 import './App.css';
 
 class App extends Component {
@@ -11,6 +12,7 @@ class App extends Component {
       <div >
         <Menu />
         <Header />
+        <Table />
         <FlatViews />
         <Contact />
 
