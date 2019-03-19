e9c4907cb28344ae0fc93377ef56c61d4ebdda72
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-03-19 13:58:34 +0100

    create actions

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,4 +1,4 @@
-import { SWITCH_VIEW, SORT_COLUMN } from './actions-type/actions-type'
+import { SWITCH_VIEW, SORT_COLUMN, VISIBLE_PARTERRE, VISIBLE_FIRST_FLOOR, VISIBLE_GARDEN, VISIBLE_LOFT } from './actions-type/actions-type'
 
 
 export const switchView = payload => {
@@ -10,6 +10,31 @@ export const switchView = payload => {
 export const sortColumn = payload => {
     return {
         type: SORT_COLUMN,
+        payload
+    }
+}
+
+export const visibleParterre = payload => {
+    return {
+        type: VISIBLE_PARTERRE,
+        payload
+    }
+}
+export const visibleFirstFloor = payload => {
+    return {
+        type: VISIBLE_FIRST_FLOOR,
+        payload
+    }
+}
+export const visibleLoft = payload => {
+    return {
+        type: VISIBLE_LOFT,
+        payload
+    }
+}
+export const visibleGarden = payload => {
+    return {
+        type: VISIBLE_GARDEN,
         payload
     }
 }
