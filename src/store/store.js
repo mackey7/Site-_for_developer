01d67fe338a0f46157fbe3573e9e3f54e048a495
Maciej Bogdanski <m.bogdanskii@gmail.com>
2019-03-21 12:36:42 +0100

    c add online version for readme.md

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,8 @@
 
 import { createStore } from 'redux'
-// import reducers from '../reducers';
 // import { composeWithDevTools } from "redux-devtools-extension";
-import rootReducer from "../reducers/rootReducer";
+import rootReducer from "../reducers/";
 
-// import tableReducer from '../reducers/tableReducer';
-// import flatViewsDate from '../reducers/flatViewsReducer'
 
 const store = createStore(rootReducer);
 export default store
